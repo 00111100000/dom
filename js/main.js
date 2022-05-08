2d6eb0e978653ce28c75c46034f03a5d5cbee561
00111100000 <104298321+00111100000@users.noreply.github.com>
2022-05-08 19:26:09 +0300

    dom

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/04.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/03.svg" alt="prev arrow"></button>',
+    });
+});
